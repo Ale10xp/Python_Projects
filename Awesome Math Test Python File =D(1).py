7de9414bc0bbae8ef8a19aef24867999da97569e
Alexandre Silveira <66680378+Ale10xp@users.noreply.github.com>
2023-01-25 22:57:22 -0300

    Updated MathGame by OpenAI ChatGPT

--- Awesome Math Test Python File =D(1).py
+++ Awesome Math Test Python File =D(1).py
@@ -1,11 +1,10 @@
 import random
 import math
 
-def expo(a, b):
-    return a**b
+def exponentiation(number, exponent):
+    return number**exponent
 
 def equationGeneration(x, a, b):
-    
     c = ( ((a*(x**2)) + (b*x)) *-1)
     if c >= 0:
         c = "+" + str(c)
@@ -25,39 +24,46 @@ def secondSolutionFinder(a, b, c):
     xd = ( ( ((-1)* b) - math.sqrt((b**2) - (4*a*c) ))/2*a )      
     return xd
 
-    
+print("Welcome to the math game!")
+print("In this game, you will be presented with a number and an exponent and you have to guess the result of raising the number to that exponent.")
+print("Then, you will be presented with a quadratic equation and you have to guess the solutions to the equation. ")
 
-#result = expo(input("The number: "), input("Elevated to: "))
+score = 0
 
-first = random.randint(-2, 2)
-second = random.randint(1, 3)
-                        
-result = expo(first, second)
-                        
-print(" the number: " + str(first) + " elevated to " + str(second))
-guess = input("ITS.....: ")
+for level in range(1, 4):
+    print("Level ", level)
+    print("Guess the result of raising the number to that exponent")
+    first = random.randint(-2, 2)
+    second = random.randint(1, 3)
 
+    result = exponentiation(first, second)
 
-if guess == result:
-    print("CORRET! ITS: " + str(result))
-else:
-    print("no =(.... it just: " + str(result))
+    print("The number: " + str(first) + " elevated to " + str(second))
+    guess = input("ITS.....: ")
+    try:
+        guess = int(guess)
+        if guess == result:
+            print("CORRECT! ITS: " + str(result))
+            score +=1
+        else:
+            print("no =(.... it just: " + str(result))
+    except ValueError:
+        print("You should enter a number")
 
+    print("an Equation for the (first - second) number: " + equationGeneration(first - second, first, second) + "\n Find the two square of.... ")
 
-print("an Equation for the (first - second) number: " + equationGeneration(first - second, first, second) + "\n Find the two square of.... ")
+    FirstGuess = input("First Square: ")
+    SecondGuess = input("Second Square: ")
 
-FirstGuess = input("First Square: ")
-SecondGuess = input("Second Square: ")
+    f_Number = firstSolutionFinder(first, second, Cgenerator(first - second, first, second))
+    s_Number = secondSolutionFinder(first, second, Cgenerator(first - second, first, second))
 
-f_Number = firstSolutionFinder(first, second, Cgenerator(first - second, first, second))
-s_Number = secondSolutionFinder(first, second, Cgenerator(first - second, first, second))
-
-if FirstGuess == f_Number:
-    print("EXACTLY!! THE FIRST SQUARE IS: "  + str(f_Number))
-else:
-    print("no... the first is: " + str(f_Number))
-
-if SecondGuess == s_Number:
-    print("and... CONGRATS the Second SQUARE IS: "  + str(s_Number))
-else:
-    print("and no... the second is: " + str(s_Number))
+    try:
+        FirstGuess = float(FirstGuess)
+        if FirstGuess == f_Number:
+            print("EXACTLY!! THE FIRST SQUARE IS: "  + str(f_Number))
+            score +=1
+        else:
+            print("no... the first is: " + str(f_Number))
+    except ValueError:
+        print("You should enter a number")
